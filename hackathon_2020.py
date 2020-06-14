aa5ae5174936b4efdf8edb4ae4e7c76c5b94cfef
sharkman121256 <66879447+sharkman121256@users.noreply.github.com>
2020-06-14 12:50:38 -0700

    Update hackathon_2020.py

--- hackathon_2020.py
+++ hackathon_2020.py
@@ -1,9 +1,9 @@
-print("Hi, What would you like to know on COVID-19?")
-keywords = ["joke", "stats", "status", "covid-19", "covid", "thanks", "hospital", "symptoms", "symptom"]
+print("Hi, I am Info_Bot, What would you like to know on COVID-19 or hear a joke?")
+keywords = ["joke", "stats", "status", "covid-19", "covid", "thanks", "hospital", "symptoms", "symptom","hi"]
 run = True
 while(run):
   userInput = input()
-  if userInput.lower() == "done" :
+  if userInput.lower() == "bye" :
     print("See ya later!")
     run = False
   else:
@@ -16,14 +16,11 @@ while(run):
           print("Pick a number 1, 2, 3, ")
           joke_input = input()
           if joke_input == '1':
-            print("Why do we tell actors to “break a leg?")
-            print("Because every play has cast.")
+            printValue="Why do we tell actors to “break a leg?\nBecause every play has cast."
           elif joke_input == '2':
-            print("What do dentists call a astronaut\'s cavity")
-            print("A black hole!")
+            printValue = "What do dentists call a astronaut\'s cavity\nA black hole!"
           if joke_input == '3':
-            print("What do you get when you cross a snowman with a vampire?")
-            print("A Frostbite")
+            printValue = "What do you get when you cross a snowman with a vampire?\nA Frostbite"
           break
         elif(match == "hospital"):
           printValue = "One of the hopitals in Silicon Valley is the West Valley Center\nLink:\nsutterhealth.org/find-location/facility/west-valley-center "
@@ -38,6 +35,8 @@ while(run):
           printValue = "Happy to help !"
         if(match == "stats" or match == "status"):
           printValue="You can find all the covid 19 stats in this website https://www.worldometers.info/coronavirus/"
+        elif(match == 'hi'):
+          printValue = "Hi, I am Info_Bot, What would you like to know on COVID-19 or hear a joke?"
           break
     if(printValue == ""):
       printValue = "Sorry, I do not know how to help at this moment."
